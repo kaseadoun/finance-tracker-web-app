ab5541a86f6e84eedab99173d81c24ac1f8c1fa9
Karsten <karstenkleung@gmail.com>
2024-10-05 18:31:19 -0400

    Added some nav elements, anchors to home and about

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
 function Header() {
     return(
         <header>
-
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
