d84127942c75189495d42012d3f7fc647a370242
Karsten <karstenkleung@gmail.com>
2024-10-30 16:59:16 -0400

    Added income route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import expenses from "./routes/expense.js";
+import incomes from "./routes/imcome.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/expenses", expenses);
+app.use("/incomes", incomes);
 
 // Start the Express Server
 app.listen(PORT, () => {
