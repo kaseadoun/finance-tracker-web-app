a802ac4771c041354a190a3c38e610a968028763
Karsten <karstenkleung@gmail.com>
2024-10-30 16:59:30 -0400

    Fixed comment

--- server/routes/expense.js
+++ server/routes/expense.js
@@ -6,7 +6,7 @@ import { ObjectId } from "mongodb";
 
 // Router is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record
+// The router will be added as a middleware and will take control of requests starting with path /
 const router = express.Router();
 
 // This section will help you get a list of all the records.
