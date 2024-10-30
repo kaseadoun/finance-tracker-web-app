649c3e41d22ef0c8c91631d0e7e05848ede694db
Karsten <karstenkleung@gmail.com>
2024-10-30 16:59:58 -0400

    Removed comment

--- server/routes/expense.js
+++ server/routes/expense.js
@@ -11,7 +11,6 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-  console.log("Trying");
   let collection = await db.collection("expenses");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
