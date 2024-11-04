08df0e4db8aa2936050f00cdcc32f20e24626e97
Karsten <karstenkleung@gmail.com>
2024-11-04 13:50:34 -0500

    Created login and signup page

--- client/src/app/pages/Login.tsx
+++ client/src/app/pages/Login.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
-    return(
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    function handleLogin(e: React.ChangeEvent<HTMLInputElement>) {
+        e.preventDefault();
+        return;
+    }
+
+    return (
         <>
             <h1>Login</h1>
+            <div className="flex flex-col max-w-md h-3/6 p-5 gap-5">
+                <form className="flex flex-col h-3/6 p-5 gap-5">
+                    <input
+                        type="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className="flex">
+                        <button>Login</button>
+                        <button><Link to="/">Cancel</Link></button>
+                    </div>
+                </form>
+                <p><Link to="/signup">Don't have an account? Sign up!</Link></p>
+            </div>
         </>
     );
 }
--- client/src/app/pages/Signup.tsx
+++ client/src/app/pages/Signup.tsx
@@ -1,9 +1,46 @@
-function Signup() {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
     return(
         <>
             <h1>Sign Up</h1>
+            <div className="flex flex-col max-w-md h-3/6 p-5 gap-5">
+                <form className="flex flex-col h-3/6 p-5 gap-5">
+                    <input 
+                        value={firstName}
+                        placeholder="First Name"
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                    <input 
+                        value={lastName}
+                        placeholder="Last Name"
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                    <input 
+                        type="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className="flex">
+                        <button>Sign Up</button>
+                        <button><Link to="/">Cancel</Link></button>
+                    </div>
+                </form>
+                <Link to="/login">Already have an account?</Link>
+            </div>
         </>
     );
 }
-
-export default Signup;
