5f41be0b4e521901d9f1d7c9ce44e6ee28c78033
Karsten <karstenkleung@gmail.com>
2024-11-06 19:56:04 -0500

    Created file to create json web token

---
+++ server/util/SecretToken.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import jwt from "jsonwebtoken";
+
+export default function createSecretToken(id) {
+    return jwt.sign({ id }, process.env.TOKEN_KEY, {
+        expiresIn: 3 * 24 * 60 * 60,
+    });
+};
