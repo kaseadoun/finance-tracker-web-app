105ae8ac24fbbf9a39d721119e435ff508330a8d
Karsten <karstenkleung@gmail.com>
2024-11-08 16:51:04 -0500

    Started secure layout for auth

---
+++ client/src/app/routes/SecureRoutes.tsx
@@ -0,0 +1,18 @@
+import SecureLayout from "../layouts/SecureLayout";
+import ErrorPage from "../pages/Error";
+import Dashboard from "../pages/Dashboard";
+import Expense from "../pages/expenses/index";
+
+export const secureRoutes = {
+    path: '/',
+    element: <SecureLayout />,
+    children: [
+        {
+            path:'dashboard',
+            title: "Dashboard",
+            element: <Dashboard />,
+            errorElement: <ErrorPage />
+        }
+    ]
+
+}
