923be8aa42fb03dba41d1581740f532951916d6f
Karsten <karstenkleung@gmail.com>
2024-11-13 16:52:06 -0500

    Started separate expense page

--- client/src/app/pages/expenses/index.tsx
+++ client/src/app/pages/expenses/index.tsx
@@ -1,6 +1,12 @@
 export default function ExpensePage() {
     return(
         <>
+            <table>
+                <tr>
+                    <th>Expense</th>
+                    <th>Amount</th>
+                </tr>
+            </table>
         </>
     );
 }
